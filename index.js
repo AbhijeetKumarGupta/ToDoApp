08e4cd7eddd8d0104a3a286fee9acc23e19b31e3
Abhijeet Kumar Gupta <84043354+AbhijeetKumarGupta@users.noreply.github.com>
2021-06-20 11:35:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,24 @@ var unList = document.getElementById("todoList");
 var addBut = document.getElementById("addButton");
 var mainDiv = document.getElementById("list");
 
+var contextMenuDiv = document.getElementById("cm");
+var editCM = document.createElement("p");
+editCM.className = "cmItems";
+editCM.innerHTML = "Edit";
+var seperationDiv = document.createElement("div");
+seperationDiv.style.backgroundColor = "black";
+seperationDiv.style.height = "1px";
+var deleteCM = document.createElement("p");
+deleteCM.innerHTML = "Delete";
+deleteCM.className = "cmItems";
+contextMenuDiv.append(editCM);
+contextMenuDiv.append(seperationDiv);
+contextMenuDiv.append(deleteCM);
+
+addEventListener("click", function () {
+  contextMenuDiv.style.display = "none";
+});
+
 addBut.addEventListener("click", function (e) {
   if (textBox.value != "") {
     var divListItem = document.createElement("div");
@@ -26,7 +44,7 @@ addBut.addEventListener("click", function (e) {
     deleteIcon.className = "fas fa-trash";
     var checkIcon = document.createElement("i");
     checkIcon.className = "fas fa-check";
-    checkIcon.style.visibility = "hidden";
+    checkIcon.style.display = "none";
 
     divIcons.prepend(checkIcon);
     divIcons.append(editIcon);
@@ -48,7 +66,11 @@ addBut.addEventListener("click", function (e) {
     });
 
     editIcon.addEventListener("click", function () {
-      checkIcon.style.visibility = "visible";
+      editCM.style.pointerEvents = "none";
+      deleteCM.style.pointerEvents = "none";
+      deleteIcon.style.pointerEvents = "none";
+      checkIcon.style.display = "unset";
+      editIcon.style.display = "none";
       var tempItemHolder = itemHolder.innerHTML;
       var tempItem = document.createElement("input");
       tempItem.className = "tempInput";
@@ -62,8 +84,56 @@ addBut.addEventListener("click", function (e) {
         itemHolder = document.createElement("h3");
         itemHolder.innerHTML = valTemp;
         divItemDate.prepend(itemHolder);
-        checkIcon.style.visibility = "hidden";
         dateHolder.innerHTML = new Date().toUTCString();
+        checkIcon.style.display = "none";
+        editIcon.style.display = "unset";
+        editCM.style.pointerEvents = "auto";
+        deleteIcon.style.pointerEvents = "auto";
+        deleteCM.style.pointerEvents = "auto";
+      });
+    });
+
+    divListItem.addEventListener("contextmenu", function (e) {
+      e.preventDefault();
+      contextMenuDiv.style.left = e.layerX + "px";
+      contextMenuDiv.style.top = e.layerY + "px";
+      contextMenuDiv.style.display = "unset";
+      var count = 1;
+      editCM.addEventListener("click", function (e) {
+        if (count == 1) {
+          editCM.style.pointerEvents = "none";
+          deleteCM.style.pointerEvents = "none";
+          deleteIcon.style.pointerEvents = "none";
+          checkIcon.style.display = "unset";
+          editIcon.style.display = "none";
+          var tempItemHolder = itemHolder.innerHTML;
+          var tempItem = document.createElement("input");
+          tempItem.className = "tempInput";
+          tempItem.value = tempItemHolder;
+          itemHolder.replaceWith(tempItem);
+
+          checkIcon.addEventListener("click", function () {
+            var valTemp = tempItem.value;
+            tempItem.remove();
+            itemHolder.remove();
+            itemHolder = document.createElement("h3");
+            itemHolder.innerHTML = valTemp;
+            divItemDate.prepend(itemHolder);
+            dateHolder.innerHTML = new Date().toUTCString();
+            checkIcon.style.display = "none";
+            editIcon.style.display = "unset";
+            editCM.style.pointerEvents = "auto";
+            deleteCM.style.pointerEvents = "auto";
+            deleteIcon.style.pointerEvents = "auto";
+          });
+        }
+        count++;
+      });
+      deleteCM.addEventListener("click", function () {
+        if (count == 1) {
+          divListItem.remove();
+        }
+        count++;
       });
     });
   }
